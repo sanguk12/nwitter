5305b1238f1989348d23db11fdf8ebd334b86ba2
han <magicspells2031@gmail.com>
2023-10-13 16:32:03 +0900

    signup 페이지 생성

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,18 +1,22 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import SignUp from "routes/SignUp";
 
 const AppRouter = ({isLoggedIn}) => {
     return (
-        <Router>
+        <BrowserRouter>
             <Routes>
                 { isLoggedIn ? (
                     <Route exact path="/" element={<Home/>} />
                 ) : (
-                    <Route exact path="/" element={<Auth/>} />
+                    <>
+                        <Route exact path="/signup" element={<SignUp />} />
+                        <Route exact path="/" element={<Auth />} />
+                    </>
                 )}
             </Routes>
-        </Router>
+        </BrowserRouter>
     );
 };
 
